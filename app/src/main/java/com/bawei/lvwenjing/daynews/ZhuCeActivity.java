f04be3da95589779f884279000bc80c7368576d4
nuanxuehanfeng <3237970009@qq.com>
2017-05-10 17:01:25 +0800

    21

---
+++ app/src/main/java/com/bawei/lvwenjing/daynews/ZhuCeActivity.java
@@ -0,0 +1,13 @@
+package com.bawei.lvwenjing.daynews;
+
+import android.app.Activity;
+import android.os.Bundle;
+//王学士，注册界面
+public class ZhuCeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zhu_ce);
+    }
+}
