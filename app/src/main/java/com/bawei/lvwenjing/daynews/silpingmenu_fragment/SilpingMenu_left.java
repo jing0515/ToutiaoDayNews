9597418bc3145518b499d3261d85df07f9139ca6
nuanxuehanfeng <3237970009@qq.com>
2017-05-10 14:58:50 +0800

    21

--- app/src/main/java/com/bawei/lvwenjing/daynews/silpingmenu_fragment/SilpingMenu_left.java
+++ app/src/main/java/com/bawei/lvwenjing/daynews/silpingmenu_fragment/SilpingMenu_left.java
@@ -1,13 +1,22 @@
 package com.bawei.lvwenjing.daynews.silpingmenu_fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
+import com.bawei.lvwenjing.daynews.MainActivity;
 import com.bawei.lvwenjing.daynews.R;
+import com.umeng.socialize.UMAuthListener;
+import com.umeng.socialize.UMShareAPI;
+import com.umeng.socialize.bean.SHARE_MEDIA;
+
+import java.util.Map;
 
 
 /**
@@ -16,10 +25,48 @@ import com.bawei.lvwenjing.daynews.R;
 
 public class SilpingMenu_left extends Fragment {
 
+    private ImageButton tengxunweibo;
+    private ImageButton qq;
+    private ImageButton weibo;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.sildingmenu_left, container, false);
+        inintView(view);
+
         return view;
     }
+
+    //王学士   初始化视图
+    private void inintView(View view) {
+        tengxunweibo = (ImageButton) view.findViewById(R.id.silpingmenu_rigth_tengxun_weibo);
+        qq = (ImageButton) view.findViewById(R.id.silpingmenu_rigth_QQ);
+        weibo = (ImageButton) view.findViewById(R.id.silpingmenu_rigth_weibo);
+        tengxunweibo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MainActivity().loadweixin();
+            }
+        });
+        qq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MainActivity().loadQQ();
+            }
+        });
+        weibo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MainActivity().loadSIna();
+            }
+        });
+
+
+    }
+
+    //王学士  QQ登录
+
+
+
 }
